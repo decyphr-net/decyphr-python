ac3f2cbbfa48dc8e27aad5db263b032a1f67664f
Aaron Sinnott <aaronsnig@gmail.com>
2019-09-30 21:59:01 -0300

    Update ALLOWED_HOST

--- lang/lang/settings.py
+++ lang/lang/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.16']
 
 
 # Application definition

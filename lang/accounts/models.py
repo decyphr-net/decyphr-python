d378b25c8a1d91d9d120283e91a1dc28cbbd7855
Aaron Sinnott <aaronsnig@gmail.com>
2019-10-20 21:55:56 -0300

    Add default value to user model

--- lang/accounts/models.py
+++ lang/accounts/models.py
@@ -8,10 +8,12 @@ class UserProfile(AbstractUser):
     first_language = models.ForeignKey(
         Language,
         related_name='first_language',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        default=2
     )
     language_being_learned = models.ForeignKey(
         Language,
         related_name='language_being_learned',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        default=1
     )

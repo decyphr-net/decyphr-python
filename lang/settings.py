93c8e99d1837065a0f1437b49c73543c51486241
Aaron Sinnott <aaronsnig@gmail.com>
2020-01-08 20:18:33 -0300

    Simplify settings

--- lang/settings.py
+++ lang/settings.py
@@ -81,16 +81,10 @@ WSGI_APPLICATION = 'lang.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {}
-
-if os.getenv('DATABASE_URL'):
-    DATABASES['default'] = dj_database_url.parse(os.getenv('DATABASE_URL'))
-else:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
 
 
 # Password validation

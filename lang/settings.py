6f0f8187f1a62ae4e8fcd3c883a40ccdffa2c18a
Aaron Sinnott <aaronsnig@gmail.com>
2020-01-16 14:14:58 -0300

    Update settings to use database URI

--- lang/settings.py
+++ lang/settings.py
@@ -85,12 +85,16 @@ WSGI_APPLICATION = "lang.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation

ee0c0c3ec758344bf97fcb06e044fce67c436387
Aaron Sinnott <aaronsnig@gmail.com>
2020-01-16 14:30:25 -0300

    Give name to translator endpoint

--- translator/urls.py
+++ translator/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from translator import views
 
 urlpatterns = [
-    path("", views.TranslatorView.as_view()),
+    path("", views.TranslatorView.as_view(), name="translate"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

a776b6c514ba42c178d38d9d2301c3045ee2dbfc
Aaron Sinnott <aaronsnig@gmail.com>
2020-03-02 15:36:40 -0300

    Update the serializer to return relationships

--- reading_list/serializers.py
+++ reading_list/serializers.py
@@ -3,7 +3,9 @@ from reading_list.models import ReadingList
 
 
 class ReadingListSerializer(serializers.ModelSerializer):
+    user = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = ReadingList
+        depth = 1
         fields = ["user", "book"]

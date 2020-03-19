a4a7617f28b8f9fd319c0fd3fb1ccc4a956a7871
Aaron Sinnott <aaronsnig@gmail.com>
2020-03-19 10:25:47 -0300

    Add unique validation to serializer

--- reading_list/serializers.py
+++ reading_list/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 from reading_list.models import ReadingList
 from accounts.models import UserProfile
 from books.models import Book
@@ -18,3 +19,16 @@ class AddToReadingListSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReadingList
         fields = ["user", "book"]
+        validators = [
+            UniqueTogetherValidator(
+                queryset=ReadingList.objects.all(),
+                fields=("user", "book")
+            )
+        ]
+    
+    def create(self, validated_data):
+        self.is_valid(raise_exception=True)
+        reading_list = super(AddToReadingListSerializer, self).create(validated_data)
+        reading_list.save()
+        return reading_list
+        

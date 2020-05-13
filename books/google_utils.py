6afa62618967adfcb6147868c0e33511d752f775
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-13 17:03:40 -0300

    Update the google API interface to parse all data not just one record

--- books/google_utils.py
+++ books/google_utils.py
@@ -2,6 +2,7 @@
 A utility module that used to retrieve information from the Google
 Books API.
 """
+from datetime import datetime
 import requests
 from django.conf import settings
 from languages.models import Language
@@ -27,32 +28,34 @@ def _construct_url(name, lang):
 
 
 def parse_book_data(data, lang):
-    book = data["volumeInfo"]
-    try:
-        title = "" if book.get("title") is None else book.get("title")
-        authors = "" if book.get("authors") is None else book.get("authors")
-        publisher = "" if book.get("publisher") is None else book.get("publisher")
-        publish_date = "" if book.get("publishedDate") is None else book.get("publishedDate")
-        description = "" if book.get("description") is None else book.get("description")
-        language = Language.objects.get(short_code=lang)
-        category = "" if book.get("mainCategory") is None else book.get("mainCategory")
-        small_thumbnail = "" if book.get("imageLinks") is None else book.get("imageLinks").get("smallThumbnail")
-        thumbnail = "" if book.get("imageLinks") is None else book.get("imageLinks").get("thumbnail")
-    except KeyError:
-        return None
-
-    book_dict = {
-        "title": title,
-        "author": authors,
-        "publisher": publisher,
-        "publish_date": publish_date,
-        "description": description,
-        "language": language,
-        "category": category,
-        "small_thumbnail": small_thumbnail,
-        "thumbnail": thumbnail
-    }
-    return book_dict
+    books = []
+    for book in books:
+        book = book["volumeInfo"]
+        try:
+            title = "" if book.get("title") is None else book.get("title")
+            authors = "" if book.get("authors") is None else book.get("authors")
+            publisher = "" if book.get("publisher") is None else book.get("publisher")
+            publish_date = "" if book.get("publishedDate") is None else book.get("publishedDate")
+            description = "" if book.get("description") is None else book.get("description")
+            language = Language.objects.get(short_code=lang)
+            category = "" if book.get("mainCategory") is None else book.get("mainCategory")
+            small_thumbnail = "" if book.get("imageLinks") is None else book.get("imageLinks").get("smallThumbnail")
+            thumbnail = "" if book.get("imageLinks") is None else book.get("imageLinks").get("thumbnail")
+        except KeyError:
+            return None
+        
+        books.append({
+            "title": title,
+            "author": str(authors),
+            "publisher": publisher,
+            "publish_date": datetime.today().strftime('%Y-%m-%d'),
+            "description": description,
+            "language": language.id,
+            "category": category,
+            "small_thumbnail": small_thumbnail,
+            "thumbnail": thumbnail
+        })
+    return books
 
 
 def get_books(name, language_code):

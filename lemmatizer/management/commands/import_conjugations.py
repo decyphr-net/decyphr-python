187392883b0d815ab83db60c22856d8e42f47506
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-14 11:07:16 -0300

    Update lemmatizer to create tenses and moods if they don't exist

--- lemmatizer/management/commands/import_conjugations.py
+++ lemmatizer/management/commands/import_conjugations.py
@@ -1,6 +1,6 @@
 from verbecc import Conjugator
 from django.core.management.base import BaseCommand, CommandError
-from lemmatizer.models import Verb, Form, Tense
+from lemmatizer.models import Verb, Form, Tense, Mood
 from languages.models import Language
 
 
@@ -23,11 +23,14 @@ class Command(BaseCommand):
         cg = Conjugator(lang="pt")
         for verb in verbs:
             verb_name = self.get_verb_conjugations(cg, verb.name)
+            print(verb_name)
             for mood_name, mood in self.get_verb_moods(verb_name):
+                mood_instance = Mood.objects.get_or_create(name=mood_name)
                 for tense_name, tense in self.get_table(mood):
-                    tense_name = Tense.objects.get(name=tense_name, mood__name=mood_name)
+                    tense_name = Tense.objects.get_or_create(
+                        name=tense_name, mood=mood_instance[0], language=Language.objects.get(id=1))
                     for item in tense:
                         if item == "-":
                             continue
-                        new_form = Form(form=item, verb=verb, tense=tense_name)
+                        new_form = Form(form=item, verb=verb, tense=tense_name[0])
                         new_form.save()

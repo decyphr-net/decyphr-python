b0b6622c528a1124dfdc622c5e5371ebd3243ebb
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-14 17:48:43 -0300

    Add some docstrings

--- books/models.py
+++ books/models.py
@@ -1,3 +1,9 @@
+"""
+The Book Model
+
+This model will store all necessary information pertaining to each individual
+book.
+"""
 from datetime import datetime
 from django.db import models
 from languages.models import Language
@@ -8,7 +14,8 @@ class Book(models.Model):
     title = models.CharField(max_length=150, null=False, blank=False)
     author = models.CharField(max_length=150, null=False, blank=False)
     publisher = models.CharField(max_length=150, null=True, blank=True)
-    publish_date = models.DateField(blank=True, null=True, default=datetime.now().today)
+    publish_date = models.DateField(
+        blank=True, null=True, default=datetime.now().today)
     description = models.TextField()
     category = models.TextField(blank=True, null=True)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)

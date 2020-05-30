0bf821e5aea9ca7961f0ef15d4d9635fa91353f8
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-29 23:11:30 -0300

    Add extra tests for the books app

--- books/tests.py
+++ books/tests.py
@@ -22,27 +22,47 @@ class BooksTests(APITestCase):
     """
     fixtures = ['fixtures.json']
 
-    def test_a_client_cant_access_the_endpoint_without_a_token(self):
-        """Unauthorized usage
-
-        A client that tries to access the books endpoint recieves a 401 if they
+    def test_a_client_cant_access_create_entries_without_a_token(self):
+        """A client that tries to access the books endpoint recieves a 401 if they
         don't provide a token
         """
         url = reverse("books-list")
         response = self.client.get(url)
-        
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
     def test_a_client_can_access_the_endpoint_with_a_token(self):
-        """Authorized usage
-
-        An authorized client will be able to access the endpoint
+        """Authorized clients will be able to access the endpoint and search
+        for books
         """
         url = reverse("books-list")
         user = UserProfile.objects.first()
 
         self.client.force_authenticate(user=user)
-
         response = self.client.get(url, {"name": "harry"})
-        
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_a_client_cant_retrieve_an_individual_book_without_a_token(self):
+        """Unauthorized attempt to retrieve a book based on the ID will result
+        in a 
+        """
+        create_book_url = reverse("books-list")
+        url = reverse("books-detail", kwargs={"pk": 1})
+        self.client.get(create_book_url, {"name": "harry"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    
+    def test_a_client_can_retrieve_an_individual_book_based_on_the_id(self):
+        """An authorized user an retrieve a single book based on that book's
+        ID
+        """
+        create_book_url = reverse("books-list")
+        url = reverse("books-detail", kwargs={"pk": 1})
+        user = UserProfile.objects.first()
+
+        self.client.force_authenticate(user=user)
+        self.client.get(create_book_url, {"name": "harry"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.__dict__)
+        self.assertEqual(2, response.data["language"])

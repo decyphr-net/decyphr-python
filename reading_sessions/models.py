f6eb80b285c68e6bee04156ab9a320df3ee7705e
Aaron Sinnott <aaronsnig@gmail.com>
2020-06-18 17:07:34 -0300

    Remove user field from ReadingSession model

--- reading_sessions/models.py
+++ reading_sessions/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from accounts.models import UserProfile
 from library.models import LibraryBook
 
 
@@ -12,7 +11,6 @@ class ReadingSession(models.Model):
         ('F', 'Finished'),
     )
 
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     library_item = models.ForeignKey(LibraryBook, on_delete=models.CASCADE)
     duration = models.DurationField(null=False, blank=False)
     pages = models.FloatField()
